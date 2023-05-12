e6d100f8d01de0602d59ce54953faf679cd6f841
Eli Pruneda <pruneda.eli@gmail.com>
2023-05-11 23:32:12 -0500

    Classes built, tests started, inquirer started, html started.

---
+++ test/Employee.test.js
@@ -0,0 +1,36 @@
+const { describe } = require("yargs");
+const Employee = require("../lib/Employee");
+
+describe("Employee", ()=>{
+    describe("Initialization", ()=>{
+        it("describe functionality...",()=>{
+            // logic for testing
+
+            expect("")toEqual();
+        });
+    })
+
+    describe("getName", ()=>{
+        it("", ()=>{
+            expect().toEqual();
+        })
+    })
+
+    describe("getId", ()=>{
+        it("", ()=>{
+            expect().toEqual();
+        })
+    })
+
+    describe("getEmail", ()=>{
+        it("", ()=>{
+            expect().toEqual();
+        })
+    })
+
+    describe("getRole", ()=>{
+        it("", ()=>{
+            expect().toEqual();
+        })
+    })
+})
---
+++ test/Manager.test.js
@@ -0,0 +1,36 @@
+const { describe } = require("yargs");
+const Manager = require("../lib/Manager");
+
+describe("Manager", ()=>{
+    describe("Initialization", ()=>{
+        it("describe functionality...",()=>{
+            // logic for testing
+
+            expect("")toEqual();
+        });
+    })
+
+    describe("getName", ()=>{
+        it("", ()=>{
+            expect().toEqual();
+        })
+    })
+
+    describe("getId", ()=>{
+        it("", ()=>{
+            expect().toEqual();
+        })
+    })
+
+    describe("getEmail", ()=>{
+        it("", ()=>{
+            expect().toEqual();
+        })
+    })
+
+    describe("getRole", ()=>{
+        it("", ()=>{
+            expect().toEqual();
+        })
+    })
+})
